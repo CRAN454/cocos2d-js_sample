063b52889b689443a8b0cc09d8567c655e7b08b7
cran454 <cran454@gmail.com>
2019-12-15 14:57:07 +0900

    [master]・説明追加

--- extend_sample/src/custom-sprite.js
+++ extend_sample/src/custom-sprite.js
@@ -1,10 +1,13 @@
 // スプライト継承
 var CustomSprite = cc.Sprite.extend({
-    ctor: function (url) {
-        this._super();
-        this.initWithFile(url);
-    },
-    changeSprite: function(url) {
-      this.initWithFile(url);
-    }
+  // コンストラクタに引数を設定
+  // 引数をなしにしたい場合は括弧内の引数のurlを削除
+  ctor: function (url) {
+    this._super();
+    // 画像を設定するメソッド
+    this.initWithFile(url);
+  },
+  changeSprite: function(url) {
+    this.initWithFile(url);
+  }
 });
